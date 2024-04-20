1a2afb354e9ae127381299e30633ef7a79452c29
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-04-20 07:50:24 -0700

    ZarinPlus Section

---
+++ components/ZarinPlus.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const ZarinPlus = () => {
+    return (
+        <div>
+            <div className="zarinplus--container">
+                <img className="zarinplus--img" src="./Images/Zarinplus--img.svg" />
+                <div className="zarinplus--content">
+                    <div className="zarinplus--content__header1">زرین‌پلاس</div>
+                    <div className="zarinplus--content__header2">ابزار هوشمند سوددهی</div>
+                    <div className="zarinplus--content__text">زرین‌پلاس محصولی از زرین‌پال، فرصتی فراهم کرده است تا کسب و کارها
+                        در هر
+                        اندازه و دسته‌بندی شغلی، با در دست داشتن ابزاری
+                        قدرتمند برای وفادارسازی مشتریان و افزایش فروش محصولات خود، بتوانند کسب و کار خود را به بهترین شکل
+                        توسعه
+                        و گسترش دهند و
+                        جایگاه برترین فروشگاه منطقه را به خود اختصاص دهند.<br />
+                        <div className="zarinplus--content__btn__wrapper">
+                            <button className="zarinplus--content__btn">
+                                پیوستن به زرین پلاس
+                            </button>
+                            <a href="#" className="zarinplus--content__link">بیش تر بدانید
+                                <img className="zarinplus--content__arrow" src="Images/arrow-left.svg" />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ZarinPlus
