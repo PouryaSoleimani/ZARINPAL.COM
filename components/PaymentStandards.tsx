29faf67740531674ccd8e03b0dca2d7f74401b46
Pourya__Soleimani <pouryasoleimani93@gmail.com>
2024-04-20 07:52:50 -0700

    PaymentStandards Section

---
+++ components/PaymentStandards.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const PaymentStandards = () => {
+    return (
+        <div>
+            <div className="paymentStandars--container">
+                <h1 className="paymentStandards--header">ویژگی‌های درگاه پرداخت زرین‌پال</h1>
+
+                <p className="paymentStandards--content">درگاه پرداخت اینترنتی زرین‌پال، امکان مدیریت تراکنش‌های مالی در بستر وب
+                    را
+                    به راحتی امکان‌پذیر می‌کند و با پیاده‌سازی و
+                    اجرای راه‌کارهای هوشمندانه، باعث افزایش درصد تراکنش‌های موفق و سوددهی بیش‌تر می‌شود.</p>
+            </div>
+        </div>
+    )
+}
+
+export default PaymentStandards
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Parallax from './../components/Parallax';
 import Payment from './../components/Payment';
 import Social from './../components/Social';
 import ZarinCard from './../components/ZarinCard';
+import Payout from './../components/Payout';
+import ZarinPlus from './../components/ZarinPlus';
+import PaymentStandards from './../components/PaymentStandards';
 
 function App() {
 
@@ -11,9 +14,12 @@ function App() {
     <>
       <Header />
       <Parallax />
-      <Payment/>
-      <Social/>
-      <ZarinCard/>
+      <Payment />
+      <Social />
+      <ZarinCard />
+      <Payout />
+      <ZarinPlus />
+      <PaymentStandards />
     </>
   )
 }
