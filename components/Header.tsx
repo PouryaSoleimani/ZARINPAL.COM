f2172b3e0305342a61f3680fa93539312bb45fd3
Pourya Soleimani <pouryasoleimani93@gmail.com>
2024-06-23 16:13:07 -0700

    CSS

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,27 +2,27 @@
 
 
 const Header = () => {
-    return (
-        <>
-            <div className="header__sticky">
-                <div className="header-sticky__img">
-                    <img src="/public/header__logo.svg" />
-                </div>
-                <div className="header__list">
-                    <ul className="header__list-main">
-                        <li>محصولات <img src="/public/chevron__down.svg" alt="" className="header__list--img" /></li>
-                        <li>تعرفه ها</li>
-                        <li>توسعه دهندگان</li>
-                        <li>تماس با ما</li>
-                        <li>بیش تر<img src="/public/chevron__down.svg" alt="" className="header__list--img" /></li>
-                    </ul>
-                    <button className="headerBtn">زرین پال من</button>
+  return (
+    <>
+      <div className="header__sticky">
+        <div className="header-sticky__img">
+          <img src="/public/header__logo.svg" />
+        </div>
+        <div className="header__list">
+          <ul className="header__list-main">
+            <li>محصولات <img src="/public/chevron__down.svg" alt="" className="header__list--img" /></li>
+            <li>تعرفه ها</li>
+            <li>توسعه دهندگان</li>
+            <li>تماس با ما</li>
+            <li>بیش تر<img src="/public/chevron__down.svg" alt="" className="header__list--img" /></li>
+          </ul>
+          <button className="header__btn">زرین پال من</button>
 
-                </div>
+        </div>
 
-            </div>
-        </>
-    )
+      </div>
+    </>
+  )
 }
 
 export default Header
--- components/Parallax.tsx
+++ components/Parallax.tsx
@@ -1,38 +1,38 @@
 //^ PARALLAX
 const Parallax = () => {
-    return (
-        <div>
-            <div className="parallax__container">
-                <div className="parallax__main">
-                    <div className="parallax__content">
-                        <h2>بیش از ۱۳ سال
-                            <h2>انتخاب
-                                مطمئنِ
-                                کسب
-                                و
-                                کارهای
-                                آنلاین</h2>
-                        </h2>
-                        <p>
-                            زرین‌پال، اولین پرداخت‌یار پیشگام کشور، با به‌کارگیری
-                            سبک و استانداردهای جدید در ارائه‌ی خدمات درگاه پرداخت
-                            اینترنتی، سرویس‌های متنوعی در حوزه‌ی پرداخت الکترونیک را
-                            برای کسب و کارها ارائه کرده است؛ با این هدف که در افزایش
-                            سهم تجارت الکترونیکی در تولید ناخالص ملی و کمک به رشد و
-                            توسعه‌ی کسب‌وکارها، نقش سازنده‌‌ای ایفا کند.
-                        </p>
-                        <a className="parallax__signup__bottom">ثبت نام</a>
-                        <a className="parallax__login__bottom">ورود</a>
-                    </div>
-                    <img src="/public/home.svg" className="parallax__img" />
-                </div>
-                <div className="parallax-arrow">
-                    <img src="/public/Parallax__bottom--arrow.svg" alt="" className="parallax-arrow__bg" />
-                    <img src="/public/chevron__down.svg" alt="" className="parallax-arrow__chevron" />
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="parallax__container">
+        <div className="parallax__main">
+          <div className="parallax__content">
+            <h2>بیش از ۱۳ سال
+              <h2>انتخاب
+                مطمئنِ
+                کسب
+                و
+                کارهای
+                آنلاین</h2>
+            </h2>
+            <p>
+              زرین‌پال، اولین پرداخت‌یار پیشگام کشور، با به‌کارگیری
+              سبک و استانداردهای جدید در ارائه‌ی خدمات درگاه پرداخت
+              اینترنتی، سرویس‌های متنوعی در حوزه‌ی پرداخت الکترونیک را
+              برای کسب و کارها ارائه کرده است؛ با این هدف که در افزایش
+              سهم تجارت الکترونیکی در تولید ناخالص ملی و کمک به رشد و
+              توسعه‌ی کسب‌وکارها، نقش سازنده‌‌ای ایفا کند.
+            </p>
+            <a className="parallax__signup__bottom">ثبت نام</a>
+            <a className="parallax__login__bottom">ورود</a>
+          </div>
+          <img src="/public/home.svg" className="parallax__img" />
         </div>
-    )
+        <div className="parallax-arrow">
+          <img src="/public/Parallax__bottom--arrow.svg" alt="" className="parallax-arrow__bg" />
+          <img src="/public/chevron__down.svg" alt="" className="parallax-arrow__chevron" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Parallax
